5fa4f65cd39974a43c7c40b7c68147c7b54c670a
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-01-21 17:09:22 -0500

    Created Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,1 +1,30 @@
+import { Drawer, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import React from "react";
 
+const Sidebar: React.FC = () => {
+  const menuItems = ['Home', 'Requests', 'Volunteers', 'Resources'];
+
+  return (
+    <Drawer
+    variant="permanent"
+    sx={{
+      width: 240,
+      '& .MuiDrawer-paper': {
+        width: 240,
+        boxSizing: 'border-box',
+      },
+    }}>
+      <List>
+        {menuItems.map((item, index) => (
+          <ListItem disablePadding key={index}>
+            <ListItemButton>
+              <ListItemText primary={item} />
+            </ListItemButton>  
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  )
+}
+
+export default Sidebar;
