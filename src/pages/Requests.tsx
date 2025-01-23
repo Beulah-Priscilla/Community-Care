5e36800a1d0f90da65d17334ef07d7ff78b8cdcb
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-01-23 10:40:03 -0500

    Enhanced Requests page

--- src/pages/Requests.tsx
+++ src/pages/Requests.tsx
@@ -1,7 +1,26 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
 
 const Requests: React.FC = () => {
-  return <h2>Here you can see and submit requests for help!</h2>;
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  }
+  return (
+    <Box sx={{ maxWidth: 500, mx: 'auto', mt: 4 }}>
+      <Typography>
+        Submit a Request
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField fullWidth label="Name" margin="normal" required/>
+        <TextField fullWidth label="Location" margin="normal" required/>
+        <TextField fullWidth label="Describe your request"
+        multiline rows={4} margin="normal" required/>
+        <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+          Submit
+        </Button>
+      </form>
+    </Box>
+    )
 
 };
 
